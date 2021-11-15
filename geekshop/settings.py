9e4a55965f1d20dc5548d0a1b52e635bb7c5e973
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-11-15 05:35:30 +0500

    Lesson 4 (#4)
    * Lesson 4. Data from files
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD
    
    * Lesson 4. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
+    "authnapp",
 ]
+
+# Auth model
+AUTH_USER_MODEL = "authnapp.ShopUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -122,8 +126,6 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+MEDIA_URL = '/media/'
 
-# Media files
-MEDIA_URL = "/media/"
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
