ecabc67a97f269212ad4828a64078e33f90c4223
fominykhgreg <amigo74@mail.ru>
2021-11-18 14:19:20 +0500

    Fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "mainapp",
     "authnapp",
+    "basketapp",
 ]
 
 # Auth model
@@ -126,6 +127,6 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
