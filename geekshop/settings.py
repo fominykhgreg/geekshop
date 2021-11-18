6331a36135acd5f1cf61026ea786178a274dd38b
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-11-18 14:15:50 +0500

    Lesson 5 (#6)
    * Lesson 4. Data from files
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD
    
    * Lesson 4. Code style fix
    
    * Fix
    
    * Lesson 5. ORM filters
    
    * Lesson 5. Basket app start
    
    * Lesson 5. Basket counter
    
    * Lesson 5. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "mainapp",
     "authnapp",
+    "basketapp",
+
 ]
 
 # Auth model
@@ -126,6 +128,11 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+=======
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
